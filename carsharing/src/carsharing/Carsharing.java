4fe356ec32de6bbb861dcd94ebbdf3d64349e285
zhouzhiqiang <zhouzhiqiang@Lab06_05.itseinaudi.local>
2016-12-20 11:02:45 +0100

    carsharing combo

--- carsharing/src/carsharing/Carsharing.java
+++ carsharing/src/carsharing/Carsharing.java
@@ -21,6 +21,7 @@ public class Carsharing {
 
 	protected Shell shlCarsharing;
 	private Table table;
+	private String cf = "";
 	Database d=new Database();
 
 	/**
@@ -60,27 +61,10 @@ public class Carsharing {
 		shlCarsharing.setSize(570, 300);
 		shlCarsharing.setText("Carsharing");
 
-		Combo combo = new Combo(shlCarsharing, SWT.NONE);
+		Combo comboSocio = new Combo(shlCarsharing, SWT.NONE);
 		DateTime dateInizio = new DateTime(shlCarsharing, SWT.BORDER);
 		DateTime dateFine = new DateTime(shlCarsharing, SWT.BORDER);
 		Button btnCerca = new Button(shlCarsharing, SWT.NONE);
-		btnCerca.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				java.text.DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-				Date dataI=null;
-				Date dataF=null;
-				String cf=combo.getText();
-				try {
-					dataI = df.parse(dateInizio.getYear() + "-" + dateInizio.getMonth() + "-" + dateInizio.getDay());
-					dataF = df.parse(dateFine.getYear() + "-" + dateFine.getMonth() + "-" + dateFine.getDay());
-				} catch (ParseException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				d.cercaNoleggio(dataI,dataF,cf);
-			}
-		});
 		table = new Table(shlCarsharing, SWT.BORDER | SWT.FULL_SELECTION);
 		TableColumn tblCodiceNoleggio = new TableColumn(table, SWT.NONE);
 		TableColumn tblAuto = new TableColumn(table, SWT.NONE);
@@ -89,9 +73,14 @@ public class Carsharing {
 		TableColumn tblFine = new TableColumn(table, SWT.NONE);
 		TableColumn tblAutoRestituita = new TableColumn(table, SWT.NONE);
 
-		combo.setItems(new String[] { "RSSMRA19T54A000Z", "RSSLCA21A78A000Q", "BNCLGO68B80E111T", "VRDNNA41C66S456W",
-				"DMALDA18D91A000A" });
-		combo.setBounds(10, 10, 193, 23);
+		comboSocio.setItems(new String[] {"RSSMRA19T54A000Z\t|\tROSSI MARIO", "RSSLCA21A78A000Q\t|\tROSSI LUCA", "BNCLGO68B80E111T\t|\tBIANCHI OLGA", "VRDNNA41C66S456W\t|\tVERDI ANNA", "DMALDA18D91A000A\t|\tADAMI ALDO"});
+		comboSocio.setBounds(10, 10, 193, 23);
+		comboSocio.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				cf = comboSocio.getText();
+			}
+		});
 
 		dateInizio.setBounds(219, 10, 80, 24);
 		
@@ -122,7 +111,22 @@ public class Carsharing {
 		tblAutoRestituita.setWidth(85);
 		tblAutoRestituita.setText("auto restituita");
 		
-		
-		
+		btnCerca.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				java.text.DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+				Date dataI = null;
+				Date dataF = null;
+				
+				try {
+					dataI = df.parse(dateInizio.getYear() + "-" + dateInizio.getMonth() + "-" + dateInizio.getDay());
+					dataF = df.parse(dateFine.getYear() + "-" + dateFine.getMonth() + "-" + dateFine.getDay());
+				} catch (ParseException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				d.cercaNoleggio(dataI, dataF, cf);
+			}
+		});
 	}
 }
